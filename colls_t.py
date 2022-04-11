c5625c3f89488c87d62f083546733155923f51f5
Badchaos11 <undrk95@gmail.com>
2022-04-11 18:11:58 +0700

    dev funca

---
+++ colls_t.py
@@ -0,0 +1,212 @@
+import os
+import sys
+from datetime import datetime
+
+import dateutil.relativedelta as relativedelta
+import gspread as gd
+import numpy as np
+import pandas as pd
+import pandas_ta as pta
+import requests
+import yfinance as yf
+
+
+gc = gd.service_account(filename='Seetzzz-1cb93f64d8d7.json')
+#sh = gc.open("work_table")
+worksheet = gc.open("work_table").worksheet('Активы с Call опционами')
+
+
+d_len = worksheet.get('D2:D60')
+a_tick = worksheet.get('A2:A60')
+d = []
+a = []
+for i in range(len(a_tick)):
+
+    if (len(a_tick[i])) == 1:
+        d.append(d_len[i])
+    else:
+        break
+
+for i in range(len(a_tick)):
+    a.append(a_tick[i])
+    a[i] = str(a[i])[2:-2]
+    if a[i] == 'Ticker':
+        tmp = i
+        break
+
+print(a)
+print(tmp)
+guru_tickers = []
+for i in range(len(d)):
+    guru_tickers.append(str(d[i]))
+    guru_tickers[i] = guru_tickers[i][2:-2]
+
+print(d)
+print(guru_tickers)
+
+
+#guru_tickers = ['XPAR:UBI', 'HKSE:00941', 'HKSE:09988', 'NYSE:EBS', 'NAS:AEIS', 'NAS:VRTX', 'FRA:BAYN', 'NAS:CHKP', 'ARCA:GDX', 'ARCA:SLV', 'NYSE:TME', 'HKSE:01099', 'NAS:INTC', 'NYSE:EPD', 'ARCA:AMLP', 'NYSE:RIO', 'NYSE:BHP', 'NAS:STNE', 'NYSE:CI', 'NYSE:EVN', 'HKSE:00700', 'NAS:MKTX', 'NAS:GBOX', 'NAS:TER', 'NYSE:TDOC', 'MIL:DIA']
+
+tickers_list = []
+gf_list = []
+TOTAL_DF = pd.DataFrame()
+
+for tick in guru_tickers:
+    print(tick)
+    try:
+        data_json = requests.get(
+            f'https://api.gurufocus.com/public/user/34b27ff5b013ecb09d2eeafdf8724472:683d6c833f9571582151f19efe2278a8/stock/{tick}/summary').json()
+
+        gf = data_json['summary']['company_data']['p2gf_value']
+
+        tickers_list.append(tick)
+        gf_list.append(float(gf))
+
+    except Exception as e:
+        tickers_list.append(tick)
+        gf_list.append(0)
+        print(e)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        pass
+
+TOTAL_DF['Ticker'] = tickers_list
+TOTAL_DF['Gf_value'] = gf_list
+
+print('First')
+print(TOTAL_DF)
+
+listus = TOTAL_DF['Ticker'].tolist()
+yahoo_list = []
+for num in range(len(listus)):
+        if "TSX:" in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.TO')
+        elif 'TSXV:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.V')
+        elif 'XSWX:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.SW')
+        elif 'IST:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.IS')
+        elif 'XKLS:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.KL')
+        elif 'LSE:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.L')
+        elif 'SGX:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.SI')
+        elif 'HKSE:0' in listus[num]:
+            yahoo_list.append(listus[num].replace('HKSE:0', '')+'.HK')
+        elif 'ASX:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.AX')
+        elif 'JSE:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.JO')
+        elif 'XTAE:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.TA')
+        elif 'TSE:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.T')
+        elif 'MIC:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.ME')
+        # elif 'MEX:' in listus[num]:
+        #     yahoo_list.append(listus[num].split(':')[1]+'.MX')
+        elif 'XMAD:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.MC')
+        elif 'FRA:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.F')
+        elif 'XAMS:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.AS')
+        elif 'XBRU:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.BR')
+        elif 'XPAR:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.PA')
+        elif 'MIL:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1]+'.MI')
+        elif 'NAS:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1])
+        elif 'NYSE:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1])
+        elif 'AMEX:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1])
+        elif 'ARCA:' in listus[num]:
+            yahoo_list.append(listus[num].split(':')[1])
+
+std_list = []
+std_30_list = []
+rsi = []
+
+for tick in yahoo_list:
+    try:
+        today = datetime.today()
+        start_vol = (datetime.today() - relativedelta.relativedelta(years=1))
+        end_vol = today
+
+        ohlc = pd.DataFrame()
+        ohlc[tick] = yf.download(tick, start_vol, end_vol)['Close']
+
+        log_df = pd.DataFrame()
+
+        log_df['returns'] = np.log(ohlc[tick] / ohlc[tick].shift(-1))
+        daily_std = np.std(log_df['returns'])
+        std = (daily_std * 252 ** 0.5)
+
+        days_30 = ohlc[tick][-10:]
+        returns_30 = np.log(days_30 / days_30.shift(-1))
+        daily_std_30 = np.std(returns_30)
+        std_30 = (daily_std_30 * 252 ** 0.5)
+
+        std_list.append(std)
+        std_30_list.append(std_30)
+
+        rsi_pr = pta.rsi(ohlc[tick])
+        rsi.append(rsi_pr[-1])
+
+        tickers_list.append(tick)
+    except:
+        pass
+
+TOTAL_DF['RSI'] = rsi
+TOTAL_DF['std'] = std_list
+TOTAL_DF['std_30'] = std_30_list
+TOTAL_DF['dif_vol'] = TOTAL_DF['std_30'] - TOTAL_DF['std']
+
+print('Second')
+print(TOTAL_DF)
+
+TOTAL_DF = TOTAL_DF.sort_values('Gf_value',ascending=False).reset_index(drop=True)
+TOTAL_DF['Gf_value_Score'] = TOTAL_DF.index.tolist()
+
+print('Third')
+print(TOTAL_DF)
+
+TOTAL_DF = TOTAL_DF.sort_values('RSI',ascending=False).reset_index(drop=True)
+TOTAL_DF['RSI_Score'] = TOTAL_DF.index.tolist()
+
+print('Fourth')
+print(TOTAL_DF)
+
+TOTAL_DF = TOTAL_DF.sort_values('dif_vol',ascending=False).reset_index(drop=True)
+TOTAL_DF['Vol_Score'] = TOTAL_DF.index.tolist()
+
+print('Fifth')
+print(TOTAL_DF)
+
+TOTAL_DF['Total_score'] = TOTAL_DF['Gf_value_Score'] + TOTAL_DF['RSI_Score'] + TOTAL_DF['Vol_Score']
+TOTAL_DF = TOTAL_DF.sort_values('Total_score').reset_index(drop=True)
+
+print('Last')
+print(TOTAL_DF)
+
+print(TOTAL_DF.columns.values.tolist())
+print(TOTAL_DF.values.tolist())
+print(TOTAL_DF.columns.values.tolist() + TOTAL_DF.values.tolist())
+
+
+#worksheet.update(f'A{tmp+3}', TOTAL_DF.values.tolist())
+
+at = worksheet.get(f'A{tmp+3}:A{tmp+12}')
+
+res = []
+for i in range(len(at)):
+    res.append(str(at[i]))
+    res[i] = res[i][2:-2]
+
+print(res)
---
+++ testing_y.py
@@ -0,0 +1,86 @@
+from func import *
+import numpy as np
+import pandas as pd
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.filters import Text
+from aiogram.utils import executor
+from googleapiclient import discovery
+from oauth2client.service_account import ServiceAccountCredentials
+import aioschedule
+import httplib2
+import mibian
+pd.options.mode.chained_assignment = None
+
+
+rows = rows_load()
+
+print('Начинаю обновление документа')
+LIST = 'Опционный портфель (short)'
+CREDENTIALS_FILE = 'Seetzzz-1cb93f64d8d7.json'
+spreadsheet_id = '1bfNJIgSEo9V5Jww1-EoUh_onba2bGY2LpDVx4aYlPzc'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
+                                                                   ['https://www.googleapis.com/auth/spreadsheets',
+                                                                    'https://www.googleapis.com/auth/drive'])
+httpAuth = credentials.authorize(httplib2.Http())
+service = discovery.build('sheets', 'v4', http=httpAuth)
+Option_WL = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=f'{LIST}!A1:W10000',
+                                                    majorDimension='COLUMNS').execute()
+df3 = pd.DataFrame()
+df3['Tickers'] = Option_WL['values'][0][1:rows]
+df3['Option Type'] = Option_WL['values'][7][1:rows]
+df3['Stock Price'] = Option_WL['values'][4][1:rows]
+df3['Stock Price'] = df3['Stock Price'].str.replace(',', '.')
+df3['Stock Price'] = df3['Stock Price'].astype(float)
+df3['Strikes'] = Option_WL['values'][9][1:rows]
+df3['Days_to_exp'] = Option_WL['values'][22][1:rows]
+df3['Days_to_exp'] = df3['Days_to_exp'].astype(int)
+df3['Ticker_Yahoo'] = Option_WL['values'][1][1:rows]
+df3['Currency'] = Option_WL['values'][2][1:rows]
+df3['Exchange'] = Option_WL['values'][3][1:rows]
+df3['Strikes'] = df3['Strikes'].str.replace(',', '.')
+df3['Strikes'] = df3['Strikes'].astype(float)
+df3['Exp Date'] = Option_WL['values'][21][1:rows]
+df3['Exp Date'] = df3['Exp Date'].str.replace('.', '-')
+df3['Exp Date'] = pd.to_datetime(df3['Exp Date'], format='%d-%m-%Y')
+df3['Exp Date'] = df3['Exp Date'].dt.date
+df3['Current Premium'] = ''
+
+print(df3.head())
+
+print(df3)
+print(yf.download('FB'))
+
+for i in range(len(df3)):
+    try:
+        ticker = df3['Ticker_Yahoo'][i]
+
+        ohlc = pd.DataFrame()
+        ohlc[f'{ticker}'] = yf.download(ticker)['Close']
+        days_30 = ohlc[f'{ticker}'][-10:]
+        returns_30 = np.log(days_30 / days_30.shift(-1))
+        daily_std_30 = np.std(returns_30)
+        std_30 = daily_std_30 * 252 ** 0.5
+
+        greeks = mibian.BS([df3['Stock Price'][i], df3['Strikes'][i], 0, df3['Days_to_exp'][i]],
+                           volatility=std_30 * 100)
+        if df3['Option Type'][i] == 'PUT':
+            df3['Current Premium'][i] = round(greeks.putPrice, 2)
+        elif df3['Option Type'][i] == 'CALL':
+            df3['Current Premium'][i] = round(greeks.callPrice, 2)
+    except:
+        pass
+
+    #gc = gd.service_account(filename='Seetzzz-1cb93f64d8d7.json')
+    #worksheet = gc.open("work_table").worksheet("Опционный портфель (short)")
+    #for i in range(len(df3)):
+    #    try:
+    #        worksheet.update(f"L{i + 2}", df3['Current Premium'][i])
+    #    except:
+    #        pass
+
+
+
+print(df3)
+
+
